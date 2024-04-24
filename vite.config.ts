c6c38fb61110fe173c5e9fb63362f1e6a1afd812
MilenaMMay <milena@growing-excellence.tech>
2024-04-24 10:40:05 +0200

    Configure public path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nachhaltigkeitskarte/',
   plugins: [
     vue(),
     vueJsx(),
