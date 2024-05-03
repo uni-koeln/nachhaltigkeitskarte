8a77558d3b52ec8add9dfc63d3bd9512db1e4538
MilenaMMay <milena@growing-excellence.tech>
2024-05-03 19:30:33 +0200

    Add more info to one of the places

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,7 +15,7 @@ interface Place {
 }
 
 const places: Place[] = [
-  { name: 'Phil Café', coordinates: L.latLng(50.92814086076388, 6.92777104434133) },
+  { name: 'Phil Café, Uni Köln', coordinates: L.latLng(50.92814086076388, 6.92777104434133) },
   { name: 'Bistro Uni E-Raum', coordinates: L.latLng(50.928439764085766, 6.929710116076665) }
 ]
 
