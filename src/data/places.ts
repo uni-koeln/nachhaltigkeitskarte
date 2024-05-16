9b28a865243b40e3d987e4fe5dc12411f0a3af82
MilenaMMay <milena@growing-excellence.tech>
2024-05-16 09:06:49 +0200

    Create workspaces for the students

--- src/data/places.ts
+++ src/data/places.ts
@@ -6,7 +6,14 @@ export interface Place {
   coordinates: LatLng
 }
 
+const arthursPlaces: Place[] = [{ name: 'Arthur', coordinates: L.latLng(50.9283, 6.928) }]
+const lucasPlaces: Place[] = [{ name: 'Luca', coordinates: L.latLng(50.9282, 6.929) }]
+const stefansPlaces: Place[] = [{ name: 'Stefan', coordinates: L.latLng(50.9283, 6.927) }]
+
 export const places: Place[] = [
   { name: 'Phil Café, Uni Köln', coordinates: L.latLng(50.92814086076388, 6.92777104434133) },
   { name: 'Bistro Uni E-Raum', coordinates: L.latLng(50.928439764085766, 6.929710116076665) }
 ]
+  .concat(arthursPlaces)
+  .concat(lucasPlaces)
+  .concat(stefansPlaces)
