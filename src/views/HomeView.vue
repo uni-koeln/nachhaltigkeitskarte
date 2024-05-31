7417af18f2cc75198d02de7b9b0599df2314b239
MilenaMMay <milena@growing-excellence.tech>
2024-05-31 11:33:00 +0200

    Explain zoom level and starting point on map

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,9 +6,12 @@ import * as L from 'leaflet'
 import type { LatLng } from 'leaflet'
 import { getTypeName, places } from '../data/places'
 
+// TODO: Dies ist der Startpunkt der Karte - setzt ihn so, dass er mittig ist
 const universityCologne: LatLng = L.latLng(50.928489708499356, 6.929532458566885)
-const zoomLevel = 18 // maximum zoom level so that we start as close as possible
-const minZoom = 15 // avoids that users zoom out of the relevant space
+// TODO: Dies ist das Start Zoom Level - setzt es so groß, dass alle Orte zu sehen sind
+// Info: Je kleiner, desto weiter weg (14 bis 18)
+const zoomLevel = 16.3 // maximum zoom level so that we start as close as possible
+const minZoom = 14 // avoids that users zoom out of the relevant space
 
 onMounted(() => {
   var sustainabilityMap = L.map('map').setView(universityCologne, zoomLevel)
