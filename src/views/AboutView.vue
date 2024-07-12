6f0c2513cad0d844b20cff6c28409aec4ca1c356
Arthur1236 <muanzarthur@outlook.com>
2024-07-12 09:51:10 +0200

    Added Impressum

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,59 +1,52 @@
-<script setup lang="ts">
-import WelcomeItem from '../components/WelcomeItem.vue'
-import DocumentationIcon from '../components/icons/IconDocumentation.vue'
-import ToolingIcon from '../components/icons/IconTooling.vue'
-import EcosystemIcon from '../components/icons/IconEcosystem.vue'
-import CommunityIcon from '../components/icons/IconCommunity.vue'
-import SupportIcon from '../components/icons/IconSupport.vue'
-</script>
-
 <template>
   <div class="about">
     <h1>Impressum</h1>
-
+    <p>Stand: Juni 2024</p>
+    <h2>Herausgeber</h2>
+    <p><span class="todo">Herausgeber:in</span></p>
     <h2>Das Nachhaltigkeitsbüro der Universität zu Köln</h2>
     <a href="https://nachhaltigkeit.uni-koeln.de" target="_blank">Link zum Nachhaltigkeitsbüro</a>
+
+    <div>
+      Nachhaltigkeitsbüro der Universität zu Köln<br>
+      Albertus Magnus Platz<br>50923 Köln<br>Tel.: 0221 / 470-1868</div>
+
+    <a href="mailto:nachhaltigkeit@verw.uni-koeln.de">nachhaltigkeit@verw.uni-koeln.de</a>
+    <p>Internet: <a href="https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit"
+        target="_blank"
+        rel="noopener noreferrer">https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit</a>
+    </p>
+
+    <p>Dr. Pamela Kilian/ Linus Ester/ Martin Speer</p>
+    <p>Diese Karte ist im Rahmen eines Service Learning Projektes ( ProfessionalCenter )
+      entstanden und von Arthur-Muanza Ndiema und Stefan Baumgärtner unter Mithilfe der
+      Dozentin <a href="https://growing-excellence.tech/about" target="_blank">Milena-Mercedes May</a> umgesetzt worden.
+    </p>
+
+    <p>Die Universität zu Köln ist eine Körperschaft des Öffentlichen Rechts. <br>
+      Sie wird durch den Rektor Professor Dr. Joybrato Mukherjee gesetzlich vertreten.</p>
+    <p>
+      Zuständige Aufsichtsbehörde:<br><br>
+      Ministerium für Kultur und Wissenschaft des Landes Nordrhein-Westfalen <br>
+      Völklinger Str. 49 <br>
+      40221 Düsseldorf <br>
+      <a href="http://www.wissenschaft.nrw.de/" target="_blank" rel="">http://www.wissenschaft.nrw.de/</a> <br> <br>
+    </p>
+    Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz: DE 123486767
+    Inhaltlich Verantwortlicher gemäß § 5 des Telemediengesetzes (TMG) und § 55 Abs. 2 des
+    Rundfunkstaatsvertrages (RStV): <span class="todo">Herausgeber:in</span>
+
+    <p>Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte externer
+      Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.
+
+      Das Nachhaltigkeitsbüro der Universität zu Köln ist eine zentrale Anlaufstelle für alle Belange
+      rund um Umwelt- und Klimaschutz sowie nachhaltige Entwicklung an der Universität.
+      Das Nachhaltigkeitsbüro verortet sich im Innovationszentrum und inspiriert damit den
+      Gründergeist und das Start-up-Ökosystem der Universität. Es erarbeitet innovative Formate,
+      um Akteur*innen aktiv zusammenzubringen, deren Kompetenzen zu entwickeln, Interesse
+      und Engagement zu fördern.</p>
   </div>
 
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Foo</template>
-    erster Text
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>Bar</template>
-    Lorem Ipsum dolor sit amat.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>Baz</template>
-    Lorem Ipsum dolor sit amat.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>FooBar</template>
-    Lorem Ipsum dolor sit amat.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>BarFoo</template>
-    Lorem Ipsum dolor sit amat.
-  </WelcomeItem>
 </template>
 
 <style></style>
