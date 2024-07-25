3349961d5a048a4a60688155a491eece9aa01a2a
MilenaMMay <milena@growing-excellence.tech>
2024-07-25 10:03:43 +0200

    Move styles from App.vue into main.css

--- src/App.vue
+++ src/App.vue
@@ -4,74 +4,26 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <div class="image-header">
+    <div class="header-uzk">
       <a href="https://www.uni-koeln.de/" target="_blank"
         ><img alt="Logo der Universität zu Köln" class="logo" src="@/assets/logo_uzk.png"
       /></a>
-      <div id="title" class="wrapper">
-      Nachhaltigkeitskarte Universität zu Köln
-      <nav class="navigation">
-        <RouterLink to="/">Karte</RouterLink>
-        <RouterLink to="/about">Info</RouterLink>
-      </nav>
+      <div id="title" class="header-content">
+        Nachhaltigkeitskarte Universität zu Köln
+        <nav class="navigation">
+          <RouterLink to="/">Karte</RouterLink>
+          <RouterLink to="/about">Info</RouterLink>
+        </nav>
+      </div>
+      <div class="header-nachhaltigkeit">
+        <a
+          href="https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit"
+          target="_blank"
+          ><img alt="Logo des Nachhaltigkeitsbüros" class="logo" src="@/assets/logo_nhb.png"
+        /></a>
+      </div>
     </div>
-      <a
-        href="https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit"
-        target="_blank"
-        ><img alt="Logo des Nachhaltigkeitsbüros" class="logo" src="@/assets/logo_nhb.png"
-      /></a>
-    </div>
-
   </header>
 
   <RouterView />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.image-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-  max-width: 15.625em;
-}
-
-.image-header a:hover {
-  background: rgba(0, 0, 0, 0);
-}
-nav {
-  width: 100%;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-#title {
-  text-align: center;
-  font-size: x-large;
-}
-</style>
