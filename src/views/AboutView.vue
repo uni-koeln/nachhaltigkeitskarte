44d5668a641b502ea4305d72432352d53d8a3dc3
Arthur1236 <muanzarthur@outlook.com>
2024-07-25 10:35:52 +0200

    Final Impressum

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,8 @@
   <div class="about">
     <h1>Impressum</h1>
     <p>Stand: Juni 2024</p>
-    <h2>Herausgeber</h2>
-    <p><span class="todo">Herausgeber:in</span></p>
+    <h2>Herausgeberin</h2>
+    <p>Dr. Pamela Kilian</p>
     <h2>Das Nachhaltigkeitsbüro der Universität zu Köln</h2>
     <a href="https://nachhaltigkeit.uni-koeln.de" target="_blank">Link zum Nachhaltigkeitsbüro</a>
 
@@ -15,15 +15,11 @@
     <a href="mailto:nachhaltigkeit@verw.uni-koeln.de">nachhaltigkeit@verw.uni-koeln.de</a>
     <p>
       Internet:
-      <a
-        href="https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit"
-        target="_blank"
-        rel="noopener noreferrer"
-        >https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit</a
-      >
+      <a href="https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit" target="_blank"
+        rel="noopener noreferrer">https://portal.uni-koeln.de/universitaet/universitaet-auf-einen-blick/nachhaltigkeit</a>
     </p>
 
-    <p>Dr. Pamela Kilian/ Linus Ester/ Martin Speer</p>
+    <p>Dr. Pamela Kilian</p>
     <p>
       Diese Karte ist im Rahmen eines Service Learning Projektes ( ProfessionalCenter ) entstanden
       und von Arthur-Muanza Ndiema und Stefan Baumgärtner unter Mithilfe der Dozentin
@@ -40,15 +36,13 @@
       Ministerium für Kultur und Wissenschaft des Landes Nordrhein-Westfalen <br />
       Völklinger Str. 49 <br />
       40221 Düsseldorf <br />
-      <a href="http://www.wissenschaft.nrw.de/" target="_blank" rel=""
-        >http://www.wissenschaft.nrw.de/</a
-      >
+      <a href="http://www.wissenschaft.nrw.de/" target="_blank" rel="">http://www.wissenschaft.nrw.de/</a>
       <br />
       <br />
     </p>
     Umsatzsteuer-Identifikationsnummer gemäß § 27a Umsatzsteuergesetz: DE 123486767 Inhaltlich
     Verantwortlicher gemäß § 5 des Telemediengesetzes (TMG) und § 55 Abs. 2 des
-    Rundfunkstaatsvertrages (RStV): <span class="todo">Herausgeber:in</span>
+    Rundfunkstaatsvertrages (RStV): Dr. Pamela Kilian
 
     <p>
       Trotz sorgfältiger inhaltlicher Kontrolle übernehmen wir keine Haftung für die Inhalte
