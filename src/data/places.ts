4a8d0d705145dd5e16f0e2a8c2842b5d4c15db3e
NHB-LE <l.ester@verw.uni-koeln.de>
2024-09-30 15:42:34 +0200

    Update places.ts

--- src/data/places.ts
+++ src/data/places.ts
@@ -46,7 +46,7 @@ export const getTypeName = (type: PlaceType): string => {
     case PlaceType.SozialeNachhaltigkeit:
       return 'Engagement und Transfer'
     case PlaceType.InitiativenEngagementAngebote:
-     return 'Studentische Initiativen und Angebote'
+     return 'Student. Initiativen/Angebote'
     case PlaceType.NachhaltigerCampus:
       return 'Nachhaltiger Campus'
     case PlaceType.ZentraleEinrichtungen:
