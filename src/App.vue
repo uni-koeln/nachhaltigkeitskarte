d08bb3f85bba2fa9ad1637ded0ba75daf629a88a
NHB-LE <l.ester@verw.uni-koeln.de>
2024-09-30 14:18:15 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
         /></a>
       </div>
       <div id="title" class="header-content">
-        Orte der Nachhaltigkeit an der Universität zu Köln
+        Orte der Nachhaltigkeit an der Universität zu Köln (Beta-Version)
         <nav class="navigation">
           <RouterLink to="/">Karte</RouterLink>
           <RouterLink to="/about">Impressum</RouterLink>
